6f9f68fd64b6b35f7c636e378d3444b81fce0a0a
Tony Thomas <tony.thomas@keyflow.se>
2022-06-13 09:43:17 +0200

    fix(Sort) Sort params are optional

--- classified_ads_crud/schemas/schemas_v1/request_args_v1.py
+++ classified_ads_crud/schemas/schemas_v1/request_args_v1.py
@@ -8,6 +8,7 @@ class RequestArgsV1(BaseSchema):
         required=False,
         validate=validate.OneOf(["creation", "price"]),
         default="creation",
+        allow_none=True,
     )
     order_by = fields.Str(
         data_key="orderBy",
@@ -15,10 +16,12 @@ class RequestArgsV1(BaseSchema):
         default="desc",
         validate=validate.OneOf(["asc", "desc"]),
         missing=marshmallow.missing,
+        allow_none=True,
     )
 
     @post_load
     def transform_sort(self, in_data, **kwargs):
-        if in_data["sort"] == "creation":
+        in_data_sort_param = in_data.get("sort", None)
+        if in_data_sort_param and in_data_sort_param == "creation":
             in_data["sort"] = "created_at"
         return in_data
